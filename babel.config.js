8129c8c160c7440ae9311ea8c408a801037879d2
Enoch Riese <enoch.riese@gmail.com>
2020-06-11 18:18:59 -0500

    add vue babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-	plugins: [
-		'@babel/plugin-syntax-dynamic-import'
-	],
-	presets: [
-		process.env.VUE_PLATFORM === 'web' ? '@vue/app' : {},
-		['@babel/env', { targets: { esmodules: true } }],
-			'@vue/babel-preset-jsx'
-	]
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
 }
